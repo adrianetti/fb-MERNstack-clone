8f93aab1c4b7d6328bdd2486ce535ccd7a8c30e5
Adrian Ambrosetti <adrian.ambrosetti@outlook.com>
2020-10-16 23:32:15 -0400

    commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxqNvdb8l4sAERBBydtm_GaqItqc_9Cjk",
+    authDomain: "fb-mongodb-clone.firebaseapp.com",
+    databaseURL: "https://fb-mongodb-clone.firebaseio.com",
+    projectId: "fb-mongodb-clone",
+    storageBucket: "fb-mongodb-clone.appspot.com",
+    messagingSenderId: "521433754844",
+    appId: "1:521433754844:web:25e78701c8d999598e1819",
+    measurementId: "G-E2C275PNFD"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { provider }
